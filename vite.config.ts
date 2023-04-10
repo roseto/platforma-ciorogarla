1837bfb8679d0fc4d018331a9c7f5a79b94cb996
Savin <savin@angelmario.eu>
2023-04-10 14:25:13 +0300

    Updated manfiest

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,50 @@ export default defineConfig({
 			registerType: "autoUpdate",
 			devOptions: {
 				enabled: false
+			},
+			includeAssets: [
+				"/assets/android-chrome-192x192.png",
+				"/assets/android-chrome-512x512.png",
+				"/assets/apple-touch-icon.png",
+				"/assets/favicon-16x16.png",
+				"/assets/favicon-32x32.png",
+			],
+			manifest: {
+				name: "Ciorogârla Unită",
+				short_name: "Ciorogârla Unită",
+				description: "Hai să ne modernizăm!",
+				theme_color: "#053b21",
+				background_color: "#053b21",
+				icons: [
+					{
+						src: "/assets/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/assets/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "/assets/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+					{
+						src: "/assets/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+				start_url: "/?utm_source=homescreen",
+				display: "standalone",
+				orientation: "portrait",
+				scope: "/",
+				categories: ["education", "news", "social"],
+				lang: "ro",
 			}
 		}),
 	],
