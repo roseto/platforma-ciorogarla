15c8a61e4ca134da4d020c20b90bfd553978b872
Savin <savin@angelmario.eu>
2023-04-11 23:37:06 +0300

    Preparing functions

---
+++ functions/src/index.ts
@@ -0,0 +1,8 @@
+import * as functions from "firebase-functions";
+import { defineSecret } from "firebase-functions/params";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
+export const 
