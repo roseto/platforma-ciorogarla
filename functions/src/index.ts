163fe1c9480c288896d95e844bd03fdc4e3a4d67
Savin <savin@angelmario.eu>
2023-04-12 23:36:27 +0300

    Added functions for creating and deleting users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from "firebase-functions";
-import { defineSecret } from "firebase-functions/params";
+import { sanityClient } from "../lib/sanityClient";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 
-export const 
+export const createUserDocument = functions.auth.user().onCreate((user) => {
+	sanityClient.create({
+		_type: "user",
+		displayName: user.displayName,
+		email: user.email,
+		photoURL: user.photoURL,
+		uid: user.uid,
+	});
+});
+
+export const deleteUserDocument = functions.auth.user().onDelete((user) => {
+	sanityClient.delete({
+		query: `*[_type == "user" && uid == "${user.uid}"]`,
+	})
+});
