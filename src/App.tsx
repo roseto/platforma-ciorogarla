c551f53b36b99f33c28519f8c44f0886f58947a5
Savin <savin@angelmario.eu>
2023-04-14 00:58:01 +0300

    Defered more analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from "@suid/ma
 import { darkTheme, lightTheme, commonTheme } from "./lib/theme";
 import {useIsRouting, useLocation, useRoutes} from "@solidjs/router";
 import {routes} from "./pages/routes";
-import {createEffect, createMemo, Suspense} from "solid-js";
+import {createMemo, Suspense} from "solid-js";
 import {createPalette, Palette} from "@suid/material/styles/createPalette";
 import Header from "./components/Header";
 import {useAuth, useFirebaseApp} from "solid-firebase";
-import {logEvent} from "firebase/analytics";
 import {getAuth} from "firebase/auth";
 import {useA2HS} from "./hooks/useA2HS";
-import {useAnalytics} from "./hooks/useAnalytics";
 
 
 export default function App() {
@@ -17,7 +15,6 @@ export default function App() {
 	useAuth(getAuth(app));
 	const location = useLocation();
 	const isRouting = useIsRouting();
-	const analytics = useAnalytics();
 	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 	const Routes = useRoutes(routes);
 	const palette = createMemo(() => createPalette(prefersDarkMode() ? darkTheme.palette as Palette : lightTheme.palette as Palette));
@@ -25,18 +22,6 @@ export default function App() {
 	useA2HS();
 
 	const theme = createTheme({...commonTheme, palette});
-
-	createEffect(() => {
-		const a = analytics(); // Type checking man
-
-		if (a && !isRouting()) {
-			logEvent(a, "page_view", {
-				page_title: document.title,
-				page_location: location.pathname,
-				page_path: location.pathname,
-			})
-		}
-	})
 
 	return (
 		<ThemeProvider theme={theme}>
--- src/analytics.ts
+++ src/analytics.ts
@@ -1,20 +1,29 @@
 import { initializeApp } from "firebase/app";
-import { getPerformance } from "firebase/performance";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 import {firebaseConfig} from "./lib/firebaseConfig";
 import {initializeAppCheck, ReCaptchaV3Provider} from "firebase/app-check";
 
 
 const app = initializeApp(firebaseConfig);
+const analytics = import.meta.env.MODE === "production" ? getAnalytics(app) : undefined;
 
 // @ts-ignore
 self.FIREBASE_APPCHECK_DEBUG_TOKEN = import.meta.env.MODE === "development";
 
 if (import.meta.env.MODE === "production") {
-	getPerformance(app);
 	getAnalytics(app);
 }
 
 initializeAppCheck(app, {
 	provider: new ReCaptchaV3Provider(import.meta.env.VITE_RECAPTCHA_SITE_KEY)
 });
+
+if (analytics) {
+	window.addEventListener("popstate", () => {
+		logEvent(analytics, "page_view", {
+			page_title: document.title,
+			page_location: location.pathname,
+			page_path: location.pathname,
+		})
+	})
+}
