bc586af43652cdaaee7fdead0e41736b0486ce11
Savin Angel-Mario <savin@angelmario.eu>
2023-05-24 13:43:59 +0300

    HOTFIX

--- src/pages/News/Article.tsx
+++ src/pages/News/Article.tsx
@@ -37,7 +37,7 @@ export default function ArticlePage() {
 			</Show>
 			<Container sx={{ mb: 2 }}>
 				<Stack>
-					<Typography variant="h2" sx={{ mt: data()?.cover ? 0 : 8 }}>
+					<Typography variant="h4" sx={{ mt: data()?.cover ? 0 : 8 }}>
 						{data()?.title}
 					</Typography>
 					<Typography
