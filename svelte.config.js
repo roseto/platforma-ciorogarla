62105e38fc96e371c1b203b3a796c70f9325fd46
Savin <savin@angelmario.eu>
2023-06-03 21:00:34 +0300

    Added netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 
+// @ts-ignore
 const file = fileURLToPath(new URL('package.json', import.meta.url));
 const json = readFileSync(file, 'utf8');
 const pkg = JSON.parse(json);
@@ -17,7 +18,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			edge: true,
+		}),
 		env: {
 			publicPrefix: "PUBLIC_"
 		},
