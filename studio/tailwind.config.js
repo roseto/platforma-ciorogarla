b133a7b4f7aba56adbe642b95959c98a187c41fe
Savin Angel-Mario <savin@angelmario.eu>
2023-06-13 19:55:05 +0300

    Tryout to fix studio build error

--- studio/tailwind.config.js
+++ studio/tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
