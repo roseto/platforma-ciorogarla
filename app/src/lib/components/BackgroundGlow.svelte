5e31cf44c46916bb2eddec5821d8c0ee5631afc2
Savin Angel-Mario <savin@angelmario.eu>
2023-06-30 23:24:13 +0300

    fix: moved background glow to background

--- app/src/lib/components/BackgroundGlow.svelte
+++ app/src/lib/components/BackgroundGlow.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div
-	class="absolute top-0 h-[1024px] left-0 overflow-hidden w-full z-[0] pointer-events-none"
+	class="absolute top-0 h-[1024px] left-0 overflow-hidden w-full z-[-1] pointer-events-none"
 	style="top: {top}px;"
 >
 	<div
-		class="absolute glow left-1/2 transform -translate-x-1/2 from-neutral via-transparent rounded-full w-[1024px] h-[1024px] opacity-50"
+		class="absolute glow left-1/2 transform -translate-x-1/2 from-neutral via-transparent rounded-full w-[1024px] h-[1024px] opacity-75"
 	/>
 </div>
 
