26767c0ff89eb56dd2a78fcf809b83c95cac5163
Savin Angel-Mario <savin@angelmario.eu>
2023-07-08 23:57:06 +0300

    Added redirect
    Added android app

---
+++ android/app/src/main/java/org/eu/ciorogarlaunita/app/DelegationService.java
@@ -0,0 +1,18 @@
+package org.eu.ciorogarlaunita.app;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
