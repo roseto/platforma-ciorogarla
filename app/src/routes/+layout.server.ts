57dfde714ea86b46477d17de3073908781efba24
Savin Angel-Mario <savin@angelmario.eu>
2023-07-08 23:20:20 +0300

    Added more redirects

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -6,10 +6,14 @@ const allowedWithoutAccept = ["/terms-and-conditions", "/privacy-policy", "/welc
 export const load = (async ({ locals, cookies, url }) => {
 	const session = await locals.getSession();
 	const acceptedTerms = cookies.get("accepted-terms");
-	const isSubdomain = url.origin.endsWith(".ciorogarla.eu.org");
+	const isSubdomain = url.origin.endsWith(".ciorogarla.eu.org") || url.searchParams.has("utm_source=subdomain");
+
+	if (url.host === "app.ciorogarlaunita.eu.org") {
+		throw redirect(301, "https://ciorogarla.eu.org" + url.pathname + url.search);
+	}
 
 	if (isSubdomain) {
-		throw redirect(301, "https://ciorogarla.eu.org/businesses/" + url.host.split(".")[0]);
+		throw redirect(301, "https://ciorogarla.eu.org/businesses/" + url.host.split(".")[0] + "?utm_source=subdomain");
 	}
 
 	if (!allowedWithoutAccept.includes(url.pathname) && !acceptedTerms && !isSubdomain) {
