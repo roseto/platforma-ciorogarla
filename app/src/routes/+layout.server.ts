1297e8f53a887565c265963a773efd46e079322a
Savin Angel-Mario <savin@angelmario.eu>
2023-07-13 16:44:22 +0300

    fix: exception for dev deploy

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -4,6 +4,7 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load = (async ({ locals, url }) => {
 	const session = await locals.getSession();
+	const isDevDeploy = url.origin === "dev.ciorogarla.eu.org"
 	const isSubdomain = url.origin.endsWith(".ciorogarla.eu.org");
 
 	// Since we can't use Netlify redirects, we have to do this here
@@ -11,7 +12,7 @@ export const load = (async ({ locals, url }) => {
 		throw redirect(301, "https://ciorogarla.eu.org" + url.pathname + url.search);
 	}
 
-	if (isSubdomain) {
+	if (isSubdomain && !isDevDeploy) {
 		throw redirect(
 			301,
 			"https://ciorogarla.eu.org/businesses/" + url.host.split(".")[0] + "?utm_source=subdomain",
